b4cded590257e7697e23689551a02b1ee3cd9645
Yash Pede <yashpede@ssipmt.com>
2024-10-12 13:58:26 +0530

    bugfix: appname

--- src/components/layout/title.tsx
+++ src/components/layout/title.tsx
@@ -9,7 +9,7 @@ import { Logo } from "./logo";
 
 const { useToken } = theme;
 
-const name = "Pure Pride Pharma";
+const name = import.meta.env.VITE_APP_NAME || "Shrewit";
 
 export const Title: React.FC<RefineLayoutThemedTitleProps> = ({
   collapsed,
