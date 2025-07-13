f67b2cc323210029d8bb7b3f3435627157bc00a9
Yash Pede <yashpede@ssipmt.com>
2025-07-13 12:39:13 +0530

    update: app logo

--- vite.config.ts
+++ vite.config.ts
@@ -22,16 +22,34 @@ export default defineConfig({
         short_name: "Distributor - pp",
         description: "Distributor application for purepride",
         theme_color: "#ffffff",
-        icons: [
+       icons: [
           {
-            src: "android-chrome-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "android-chrome-512x512.png",
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
